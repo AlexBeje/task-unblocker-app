03ab3f0acc0ee7491d0a305c95cface908db3261
AlexBeje <alex.bejenaru93@gmail.com>
2022-07-18 09:32:18 +0200

    5 files modified, 1 added, 2 deleted

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import "./index.css";
+import { MantineProvider } from '@mantine/core';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      theme={{ colorScheme: 'dark' }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>
-)
+);
